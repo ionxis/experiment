7abc86de45b4ab37f4ec080ab58b256a8533ad0c
ionxis <info@ionxis.com>
2026-08-09 03:03:24 +0200

    Update unauthorized response format in app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return { "message" : "unauthorized" }
+    return {
+        "code: 300,
+        "message" : "unauthorized"
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
